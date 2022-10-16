c642a66f2b85f8a87b6fd9c85f7d61e096ae7fc2
skomuravell <sainikhil_komuravelli@intuit.com>
2022-10-16 15:32:32 +0530

    Initial commit for bank app

---
+++ src/main/java/com/online/bankapp/BankAppApplication.java
@@ -0,0 +1,24 @@
+package com.online.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppApplication {
+
+	//create primary account and transaction models
+	//create savings account and transaction models
+	//create dao for primary account and its transactions
+	//create dao for savings account and its transactions
+	//create an Account service implementation
+	//create the transaction service implementation
+	//create user model and services
+	//create an Email service
+	//create an OTP service
+	//create Security by adding roles
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppApplication.class, args);
+	}
+
+}
